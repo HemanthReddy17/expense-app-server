252a13c1929ccb677145aaa1f86e24394dcae0f6
HemanthReddy17 <khreddy11@gmail.com>
2020-01-12 20:39:59 +0530

    app.js configred

--- src/app.js
+++ src/app.js
@@ -1,1 +1,20 @@
-console.log("object");
+var express = require('express');
+var bodyParser = require('body-parser');
+
+var errorLogger=require("./utilities/errorLogger")
+var requestLogger=require("./utilities/requestLogger")
+
+var routes=require("./routes/router")
+
+var cors = require('cors');
+
+var app = express();
+app.use(cors());
+app.use(bodyParser.json());
+
+app.use(requestLogger)
+app.use("/",routes)
+app.use(errorLogger)
+
+app.listen(5000)
+console.log("Server is listening at 5000")
