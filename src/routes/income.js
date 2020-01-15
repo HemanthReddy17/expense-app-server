c9fb7f98e609e69face365e81d9c3b3032d9a2bf
HemanthReddy17 <khreddy11@gmail.com>
2020-01-15 23:32:58 +0530

    user can add income

--- src/routes/income.js
+++ src/routes/income.js
@@ -11,12 +11,28 @@ router.get("/showIncomes", (req, res, next) => {
     } else {
         incomeService.getIncome(req.cookies.userDataCookie.userId).then((userIncomeData) => {
             res.send(userIncomeData)
-        }).catch(err=>next(err))
-
+        }).catch(err => next(err))
     }
-
 })
 
+
+router.post("/addIncomeData", (req, res, next) => {
+    if (req.cookies.userDataCookie === undefined) {
+        let err = new Error("Please Login...!!")
+        err.status = 401
+        next(err)
+    } else {
+        let incomeData = req.body
+        incomeData.userId = req.cookies.userDataCookie.userId
+        incomeService.addData(incomeData,req.cookies.userDataCookie.totalAmount).then(data => {
+            res.send(data)
+        })
+    }
+})
+
+// router.get("/gid",(req,res,next)=>{
+//     incomeService.gif().then(data=>{res.send(data)})
+// })
 
 
 
